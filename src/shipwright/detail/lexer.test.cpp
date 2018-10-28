dde3d974cb2cea594ead69cee143a560c59ed48e
Justin Bassett <jbassett271@gmail.com>
2018-10-27 23:11:42 -0700

    Line comments + multiple token tests

--- src/shipwright/detail/lexer.test.cpp
+++ src/shipwright/detail/lexer.test.cpp
@@ -10,6 +10,8 @@
 
 #include <catch2/catch.hpp>
 
+#include <initializer_list>
+#include <set>
 #include <sstream>
 #include <string>
 
@@ -25,6 +27,7 @@ TEST_CASE("Can parse individual tokens", "[lexer]")
         {"\n", token{"\n", token_type::newline}},
         {"(", token{"(", token_type::lparen}},
         {")", token{")", token_type::rparen}},
+        {"# some comment", token{"# some comment", token_type::line_comment}},
     }));
 
     shipwright::detail::lexer lex{input};
@@ -35,4 +38,49 @@ TEST_CASE("Can parse individual tokens", "[lexer]")
     CHECK(result == expected);
 
     REQUIRE_FALSE(lex.advance());
+}
+
+TEST_CASE("Can parse multiple tokens", "[lexer]")
+{
+    using shipwright::token;
+    using shipwright::token_type;
+
+    static auto const test_values = std::initializer_list<
+        std::tuple<std::string, token, std::set<token_type>>>{
+        {" ", token{" ", token_type::space}, std::set{token_type::space}},
+        {"\n", token{"\n", token_type::newline}, {}},
+        {"(", token{"(", token_type::lparen}, {}},
+        {")", token{")", token_type::rparen}, {}},
+        {
+            "# some comment",
+            token{"# some comment", token_type::line_comment},
+            std::set{token_type::space, token_type::lparen, token_type::rparen,
+                token_type::line_comment},
+        },
+    };
+
+    auto [first, first_expected, consumes_succeeding]
+        = GENERATE(values(test_values));
+    auto [second, second_expected, ignore] = GENERATE(values(test_values));
+    (void)ignore;
+
+    if (consumes_succeeding.count(second_expected.type)) {
+        SUCCEED();
+    } else {
+        auto const input = first + second;
+
+        shipwright::detail::lexer lex{input};
+
+        REQUIRE(lex.advance());
+        auto const first_r = lex.read();
+
+        CHECK(first_r == first_expected);
+
+        REQUIRE(lex.advance());
+        auto const second_r = lex.read();
+
+        CHECK(second_r == second_expected);
+
+        REQUIRE_FALSE(lex.advance());
+    }
 }
